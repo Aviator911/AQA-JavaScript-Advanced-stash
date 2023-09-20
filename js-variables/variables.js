84e6446e03d8ae5cdeeef7f0bf2976c0b8a30cd2
“Pavel <pavlo.m@corefy.com>
2023-09-20 18:43:01 +0300

    New file variables.js

---
+++ js-variables/variables.js
@@ -0,0 +1,14 @@
+const adminName = "Pasha";
+const userName = "Denchik";
+
+console.log(typeof adminName); 
+console.log(typeof userName);
+
+const text = `Javascript is: ${adminName}, ${userName}`;
+console.log(text);
+
+const myVariable = "123";
+console.log(myVariable); 
+
+myVariable = "321";
+console.log(myVariable); 
